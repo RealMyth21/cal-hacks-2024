cc9e81664a6f734b19b7005a7cfe4d3038afa3fe
Mithil Srungarapu <mithil@Mithils-MacBook-Air.local>
2024-06-22 11:57:24 -0700

    html template is set

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, request, url_for, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+"""
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+"""
+
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-
